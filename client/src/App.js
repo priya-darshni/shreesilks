73faf68ceaabeb1db7204e1aa52cf085dce7de5d
priyadarshni <spriyadarshni2002@gmail.com>
2023-07-02 15:21:52 +0530

    add client directory

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import ForgotPassword from './pages/Auth/ForgotPassword';
+import AdminRoute from './components/Routes/AdminRoute';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/dashboard' element={<PrivateRoute/>}>
+        <Route path='user' element={<Dashboard />}></Route>
+        </Route>
+        <Route path='/dashboard' element={<AdminRoute/>}>
+        <Route path='admin' element={<AdminDashboard />}></Route>
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
